cb5ea71e080eab0bb8b32417263e1330c34beb72
Egor Klepikov <Egor.Klepikov@jetbrains.com>
2021-01-29 14:25:00 +0300

    Added global key listener

--- src/main/java/com/color/picker/MainFrame.java
+++ src/main/java/com/color/picker/MainFrame.java
@@ -1,12 +1,18 @@
 package com.color.picker;
 
 
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
   private final JColorChooser colorPicker;
   private final PreviewPane previewPane;
+  private Robot robot;
 
   public MainFrame() {
     setSize(600, 450);
@@ -19,6 +25,14 @@ public class MainFrame extends JFrame {
     colorPicker.setSize(600, 450);
     colorPicker.setVisible(true);
     new MouseMotionListener().start();
+    try {
+      GlobalScreen.registerNativeHook();
+    } catch (NativeHookException ex) {
+      System.err.println("There was a problem registering the native hook.");
+      System.err.println(ex.getMessage());
+      System.exit(1);
+    }
+    GlobalScreen.addNativeKeyListener(new GlobalKeyListener());
     add(colorPicker);
     setVisible(true);
   }
@@ -28,8 +42,6 @@ public class MainFrame extends JFrame {
   }
 
   private class MouseMotionListener extends Thread {
-    private Robot robot;
-
     public MouseMotionListener() {
       try {
         robot = new Robot();
@@ -50,8 +62,25 @@ public class MainFrame extends JFrame {
         Color pixelColor = robot.getPixelColor((int) point.getX(), (int) point.getY());
         colorPicker.setColor(pixelColor);
         previewPane.changeCurrentColor(pixelColor);
-        previewPane.changeSelectedColor(pixelColor);
+
       }
+    }
+  }
+
+  private class GlobalKeyListener implements NativeKeyListener {
+    public void nativeKeyPressed(NativeKeyEvent e) {
+      if (e.getKeyCode() == NativeKeyEvent.VC_TAB) {
+        Point point = MouseInfo.getPointerInfo().getLocation();
+        previewPane.changeSelectedColor(robot.getPixelColor((int) point.getX(), (int) point.getY()));
+      }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+
     }
   }
 }
